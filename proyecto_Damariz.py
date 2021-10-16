8e956730c061dea71df72debc906a3620ac371bd
DamarizLicea <89052608+DamarizLicea@users.noreply.github.com>
2021-10-15 23:17:23 -0500

    Update proyecto_Damariz.py

--- proyecto_Damariz.py
+++ proyecto_Damariz.py
@@ -1,6 +1,33 @@
 """
--------------------------------------------------------------------------funciones del programa-------------------------------------------------------------------------------------
+-------------------------------------------------------------------funciones del programa---------------------------------------------------------------------
 """
+
+def imprfor(lista_pan):
+    for linea in lista_pan:
+        for columna in linea:
+            print(columna,end=" ")
+        print()
+def impwhile(lista_relleno):
+    valor=0
+    i=0
+    while i<len(lista_relleno):
+        j=0
+        while j<len(lista_relleno[0]):
+            print(lista_relleno[i][j],end=" ")
+            j=j+1
+        print()
+        i=i+1
+def impwhile(listadeco):
+    valor=0
+    i=0
+    while i<len(listadeco):
+        j=0
+        while j<len(listadeco[0]):
+            print(listadeco[i][j],end=" ")
+            j=j+1
+        print()
+        i=i+1
+
 def menu1():
     print()
     print("Hola! Bienvenido a Keikiz, nuestro menú para ordenes de pasteles es éste:")
@@ -11,14 +38,9 @@ def menu1():
               ['5','Chai'],
               ['6','Vainilla'],
               ['7','Limón']]
-    
-    def imprfor(lista_pan):
-        for linea in lista_pan:
-            for columna in linea:
-                print(columna,end=" ")
-            print()
     imprfor(lista_pan)
     print()
+
     print("El menú de rellenos es éste:")
     lista_relleno=[['1','Buttercream de Vainilla'],
                    ['2','Crema de Queso'],
@@ -26,40 +48,23 @@ def menu1():
                    ['4','Buttercream de chocolate'],
                    ['5','Buttercream de PB'],
                    ['6','Lemon Curd']]
-    def impwhile(lista_relleno):
-        valor=0
-        i=0
-        while i<len(lista_relleno):
-            j=0
-            while j<len(lista_relleno[0]):
-                print(lista_relleno[i][j],end=" ")
-                j=j+1
-            print()
-            i=i+1
     impwhile(lista_relleno)
     print()
+
     print("Nuestro menú para decoraciones es este:")
     listadeco=[['1','Fondant'],
-               ['2','Chocolate'],
-               ['3','Buttercream'],
-               ['4','Moldeado']]
-    def impwhile(listadeco):
-        valor=0
-        i=0
-        while i<len(listadeco):
-            j=0
-            while j<len(listadeco[0]):
-                print(listadeco[i][j],end=" ")
-                j=j+1
-            print()
-            i=i+1
+           ['2','Chocolate'],
+           ['3','Buttercream'],
+           ['4','Moldeado']]
     impwhile(listadeco)
     print()
+    
 def menu2():
     print("Hola! Bienvenido a Keikiz, nuestro menú por rebanadas para hoy es éste: \n1)Tarta de nuez $20 \n2)Pastel de miel $40 \n3)Pastel de rollo $35 \n4)Pastel de Brownie $30 \n5)Cheesecake de pera $45 \n6)Pay de manzana $45")
+
 def venta_directa():
     menu2()
-    producto=int(input("¿Qué va a querer?")) """amigo becario, aquí empieza mi entrega de if's para el 10/09/2021"""
+    producto=int(input("¿Qué va a querer? (Indique el número del producto): "))
     if producto==1:
         valor=20
     if producto==2:
@@ -70,12 +75,13 @@ def venta_directa():
         valor=30
     if producto==5 or producto==6:
         valor=45
-    rebanadas=int(input("¿Cuantas rebanadas?"))
+    rebanadas=int(input("¿Cuantas rebanadas? "))
     cuenta=valor*rebanadas
     print("Su cuenta es de $", cuenta)
     forma_de_pago=str(input("¿Su pago es en efectivo o tarjeta de crédito o tarjeta de débito? Si es efectivo, por favor escriba efectivo, si es tarjeta de crédito, por favor escriba credito, y si no es ninguna de las anteriores, escriba debito: "))
     if forma_de_pago==("efectivo"):
         valorventa=cuenta
+        print("Su cuenta final fue de $",valorventa)
         print("gracias, vuelva pronto")
     if forma_de_pago=="credito":
         valorventa=cuenta+(cuenta*0.10)
@@ -83,25 +89,38 @@ def venta_directa():
     if forma_de_pago=="debito":
         valorventa=cuenta+(cuenta*0.15)
         print("Su cuenta final fue de $",valorventa,",gracias,vuelva pronto")
+        
 def precioventa(producto,rebanadas):
     return producto*rebanadas
+
 def calcular_precio(personas):
     return personas*60+(personas*4)+45+((personas+60/2))
+
 def imprimir_orden(sabor,crema,deco):
     print("Usted pidio un pastel sabor ",sabor.lower(),",con crema de ",crema.lower()," y decoración de ",deco.lower())
+def calcular_corte(m50,m1,m2,m5,m10,b20,b50,b100,b200,b500,b1000):
+    m=((m50*0.5)+(m1)+(m2*2)+(m5*5)+(m10*10))
+    b=((b20*20)+(b50*50)+(b100*100)+(b200*200)+(b500*500)+(b1000*1000))
+    return m+b
 """
-===============================================================================AQUÍ YA CORRE EL PROGRAMA===========================================================================
+==================================================================AQUÍ YA CORRE EL PROGRAMA=============================================================
 """
-h=int(input(print("Hola! ¿Qué te gustaría hacer? \n1)Registrar una venta \n2)Cotizar una orden \n3)Hacer un pedido \n4)Corte de caja")))
-while h!=5:
+from datetime import datetime
+h=1
+while h<6 and h>0:
+    h=int(input("Hola! ¿Qué te gustaría hacer? \n1)Registrar una venta \n2)Cotizar una orden \n3)Hacer un pedido \n4)Corte de caja \n5)Terminar el programa \n Quisiera (indica el numero de la opción): "))
     if h==1:
+        rep=""
         venta_directa()
-        rep=input("¿Desea seguir comprando?")
-        if rep=="si" or rep=="Si":
-            continue
-        else:
-            break
+        while rep!="No" and rep!="no":
+            rep=str(input("¿Desea seguir comprando? "))
+            rep=""
+            venta_directa()
+            while rep!="No" and rep!="no":
+                rep=str(input("¿Desea seguir comprando? "))
+    
     if h==2:
+        rep=""
         print("Hola, bienvenido a nuestro sistema de cotizaciones")
         menu1()
         sabor=(str(input("¿Qué sabor de pan le interesa? ")))
@@ -110,43 +129,62 @@ while h!=5:
         deco=(str(input("¿Qué tipo de decoración le interesa? ")))
         print("Su costo es", calcular_precio(personas))
         imprimir_orden(sabor,crema,deco)
-        rep=str(input(("¿Se encuentra satisfecho con su cotización? Indiquenos Sí o No: ")))
-        if rep=="No" or rep=="No":
-            continue
-        else:
-            break
+        while rep!="No" and rep!="no":
+            rep=str(input(("¿Quiere volver a realizar su cotización? Indiquenos Sí o No: ")))
+            rep=""
+            print("Hola, bienvenido a nuestro sistema de cotizaciones")
+            menu1()
+            sabor=(str(input("¿Qué sabor de pan le interesa? ")))
+            personas=int(input("¿Para cuántas porciones sería? "))
+            crema=(str(input("¿Qué sabor de crema le interesa? ")))
+            deco=(str(input("¿Qué tipo de decoración le interesa? ")))
+            print("Su costo es", calcular_precio(personas))
+            imprimir_orden(sabor,crema,deco)
+            while rep!="No" and rep!="no":
+                rep=str(input(("¿Quiere volver a realizar su cotización? Indiquenos Sí o No: ")))
+
     if h==3:
-        print("Hola, bienvenido a nuestro sistema de ordenes")
-        menu1()
-        sabor=(str(input("¿Qué sabor de pan le interesa? ")))
-        personas=int(input("¿Para cuántas porciones sería? "))
-        crema=(str(input("¿Qué sabor de crema le interesa? ")))
-        deco=(str(input("¿Qué tipo de decoración le interesa? ")))
-        print("Su costo es", calcular_precio(personas))
-        cuenta=calcular_precio(personas)
+        rep=""
+        while rep!="Si" and rep!="si":
+            print("Hola, bienvenido a nuestro sistema de ordenes")
+            menu1()
+            sabor=(str(input("¿Qué sabor de pan le interesa? ")))
+            personas=int(input("¿Para cuántas porciones sería? "))
+            crema=(str(input("¿Qué sabor de crema le interesa? ")))
+            deco=(str(input("¿Qué tipo de decoración le interesa? ")))
+            print("Su costo es", calcular_precio(personas))
+            cuenta=calcular_precio(personas)
+            imprimir_orden(sabor,crema,deco)
+            print("Su fecha de pedido es",datetime.today().strftime('%d-%m-%Y %H:%M'))
+            rep=str(input(("¿Se encuentra satisfecho con su orden? Indiquenos Sí o No: ")))
+            
+        forma_de_pago=str(input("¿Su pago es en efectivo o tarjeta de crédito o tarjeta de débito? Si es efectivo, por favor escriba efectivo, si es tarjeta de crédito, por favor escriba credito, y si no es ninguna de las anteriores, escriba debito: "))
+        if forma_de_pago==("efectivo"):
+            valorventa=cuenta
+            print("Su cuenta final fue de $",valorventa,",gracias, vuelva pronto")
+        if forma_de_pago=="credito":
+            valorventa=cuenta+(cuenta*0.10)
+            print("Su cuenta final fue de $",valorventa,",gracias, vuelva pronto")
+        if forma_de_pago=="debito":
+            valorventa=cuenta+(cuenta*0.15)
+            print("Su cuenta final fue de $",valorventa,",gracias, vuelva pronto")
         imprimir_orden(sabor,crema,deco)
-        rep=str(input(("¿Se encuentra satisfecho con su orden? Indiquenos Sí o No: ")))
-        if rep=="no" or rep=="No":
-            continue
-        else:
-            forma_de_pago=str(input("¿Su pago es en efectivo o tarjeta de crédito o tarjeta de débito? Si es efectivo, por favor escriba efectivo, si es tarjeta de crédito, por favor escriba credito, y si no es ninguna de las anteriores, escriba debito: "))
-            if forma_de_pago==("efectivo"):
-                valorventa=cuenta
-                print("gracias, vuelva pronto")
-            if forma_de_pago=="credito":
-                valorventa=cuenta+(cuenta*0.10)
-                print("Su cuenta final fue de $",valorventa,",gracias, vuelva pronto")
-            if forma_de_pago=="debito":
-                valorventa=cuenta+(cuenta*0.15)
-                print("Su cuenta final fue de $",valorventa,",gracias, vuelva pronto")
-        imprimir_orden(sabor,crema,deco)
-        break
+        
     if h==4:
-        print("Todavia no hay nada aquí.")
-        rep=str(input(("¿Quieres cerrar el corte?")))
-        if rep=="si" or rep=="Si":
-            continue
-        else:
-            break
+        m50=int(input("¿Cuantas monedas de 50 c tenemos? "))
+        m1=int(input("¿Cuantas monedas de $1 tenemos? "))
+        m2=int(input("¿Cuantas monedas de $2 tenemos? "))
+        m5=int(input("¿Cuantas monedas de $5 tenemos? "))
+        m10=int(input("¿Cuantas monedas de $10 tenemos? "))
+        b20=int(input("¿Cuantos billetes de $20 tenemos? "))
+        b50=int(input("¿Cuantos billetes de $50 tenemos? "))
+        b100=int(input("¿Cuantos billetes de $100 tenemos? "))
+        b200=int(input("¿Cuantos billetes de $200 tenemos? "))
+        b500=int(input("¿Cuantos billetes de $500 tenemos? "))
+        b1000=int(input("¿Cuantos billetes de $1000 tenemos? "))
+        print("El corte final es: ", calcular_corte(m50,m1,m2,m5,m10,b20,b50,b100,b200,b500,b1000))
+        print("Realizaste el corte el: ", datetime.today().strftime('%d-%m-%Y %H:%M'))
+        h=6
+
 else:
-    print("Hemos cerrado, ten una buena noche.")
+    print("Adios, nos vemos pronto  :) ")

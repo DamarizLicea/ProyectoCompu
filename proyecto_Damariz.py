bda2100741e5ba9ecba029ddbdc17e1987a98437
DamarizLicea <89052608+DamarizLicea@users.noreply.github.com>
2021-10-19 16:45:23 -0500

    Update proyecto_Damariz.py

--- proyecto_Damariz.py
+++ proyecto_Damariz.py
@@ -1,13 +1,28 @@
 """
--------------------------------------------------------------------funciones del programa---------------------------------------------------------------------
+Proyecto Keikiz by Mayiz
+Damariz Licea Carrisoza A01369045
+Punto de venta, cotizador de órdenes para un negocio de pastelería.
+Despliega menú para venta regular de productos, permite cotizar una orden varias veces para que el cliente lo haga hasta estar contento.
+Después permite que el usuario haga la orden como tal de un pedido y le da opción de pagar.
+En el ultimo punto, es un contador de dinero segun el numero de monedas/billetes que el usuario ingrese.
 """
+"""
+------------------funciones del programa-------------------
+"""
+import colorama
+from colorama import Fore,Back,Style
+colorama.init()
+""" Uso de Colorama
+	Código de: https://pypi.org/project/colorama/ """
 
 def imprfor(lista_pan):
+    """ Crea el menú a partir de una matriz de 7x2 """
     for linea in lista_pan:
         for columna in linea:
             print(columna,end=" ")
         print()
 def impwhile(lista_relleno):
+    """Crea o ordena el menú (de una matriz de 7x2) gracias a un ciclo while"""
     valor=0
     i=0
     while i<len(lista_relleno):
@@ -18,6 +33,7 @@ def impwhile(lista_relleno):
         print()
         i=i+1
 def impwhile(listadeco):
+    """" Misma acción que la funcion de arriba """
     valor=0
     i=0
     while i<len(listadeco):
@@ -29,8 +45,13 @@ def impwhile(listadeco):
         i=i+1
 
 def menu1():
+    
+    """Aquí está la matriz que se usa en la primera función del programa,
+        también aquí englobo las primeras tres funciones,
+        para poder imprimir el menú entero de una sola vez"""
+    
     print()
-    print("Hola! Bienvenido a Keikiz, nuestro menú para ordenes de pasteles es éste:")
+    print(f"{Fore.GREEN}Hola! Bienvenido a Keikiz, nuestro menú para ordenes de pasteles es éste:")
     lista_pan=[['1','Red Velvet'],
               ['2','Zanahoria'],
               ['3','Gansito'],
@@ -60,9 +81,18 @@ def menu1():
     print()
     
 def menu2():
+    
+    """ Aquí está el menú que ocupo en la parte
+        de venta directa (menú de venta normal)"""
+    
     print("Hola! Bienvenido a Keikiz, nuestro menú por rebanadas para hoy es éste: \n1)Tarta de nuez $20 \n2)Pastel de miel $40 \n3)Pastel de rollo $35 \n4)Pastel de Brownie $30 \n5)Cheesecake de pera $45 \n6)Pay de manzana $45")
 
 def venta_directa():
+    
+    """ Despliega el menú para vender normalmente,
+    da el precio del producto según el número del producto en el menú,
+    y las porciones que pidió. El precio puede cambiar según la opción de pago."""
+    
     menu2()
     producto=int(input("¿Qué va a querer? (Indique el número del producto): "))
     if producto==1:
@@ -94,11 +124,18 @@ def precioventa(producto,rebanadas):
     return producto*rebanadas
 
 def calcular_precio(personas):
+    
+    """Función que calcula el precio según el
+    numero de porciones que requiere el cliente"""
+    
     return personas*60+(personas*4)+45+((personas+60/2))
 
 def imprimir_orden(sabor,crema,deco):
+    """ Reimprime y ordena la orden del cliente,
+    según los datos que nos brindó"""
     print("Usted pidio un pastel sabor ",sabor.lower(),",con crema de ",crema.lower()," y decoración de ",deco.lower())
 def calcular_corte(m50,m1,m2,m5,m10,b20,b50,b100,b200,b500,b1000):
+    """"""
     m=((m50*0.5)+(m1)+(m2*2)+(m5*5)+(m10*10))
     b=((b20*20)+(b50*50)+(b100*100)+(b200*200)+(b500*500)+(b1000*1000))
     return m+b
@@ -108,7 +145,7 @@ def calcular_corte(m50,m1,m2,m5,m10,b20,b50,b100,b200,b500,b1000):
 from datetime import datetime
 h=1
 while h<6 and h>0:
-    h=int(input("Hola! ¿Qué te gustaría hacer? \n1)Registrar una venta \n2)Cotizar una orden \n3)Hacer un pedido \n4)Corte de caja \n5)Terminar el programa \n Quisiera (indica el numero de la opción): "))
+    h=int(input("Hola! ¿Qué te gustaría hacer? \n1)Registrar una venta \n2)Cotizar una orden \n3)Hacer un pedido \n4)Corte de caja \n5)Terminar el programa \nQuisiera (indica el numero de la opción): "))
     if h==1:
         rep=""
         venta_directa()
@@ -121,7 +158,7 @@ while h<6 and h>0:
     
     if h==2:
         rep=""
-        print("Hola, bienvenido a nuestro sistema de cotizaciones")
+        print(f"{Fore.GREEN}Hola, bienvenido a nuestro sistema de cotizaciones")
         menu1()
         sabor=(str(input("¿Qué sabor de pan le interesa? ")))
         personas=int(input("¿Para cuántas porciones sería? "))
@@ -185,6 +222,8 @@ while h<6 and h>0:
         print("El corte final es: ", calcular_corte(m50,m1,m2,m5,m10,b20,b50,b100,b200,b500,b1000))
         print("Realizaste el corte el: ", datetime.today().strftime('%d-%m-%Y %H:%M'))
         h=6
+    if h==5:
+        print(Fore.GREEN+"Hola bby")
 
 else:
     print("Adios, nos vemos pronto  :) ")
